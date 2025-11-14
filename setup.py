69dc81e1e403ae41da8a86b9a9368ce598286f2d
austin <2986579627@qq.com>
2025-11-14 19:21:43 +0800

    change setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="testserve",
-    version="0.0.1",
+    version="0.0.2",
     author="austin",
     description="High performance inference engine for LLMs.",
     packages=["testserve"],
